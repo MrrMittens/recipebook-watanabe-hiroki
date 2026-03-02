a27f264c440a77a50217427505bddfe239193775
Hiroki <hiroki.watanabe@student.ateneo.edu>
2026-03-02 20:28:34 +0800

    added Profile model

--- ledger/models.py
+++ ledger/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(50)
+    bio = models.TextField(blank=True)
 
 
 class Ingredient(models.Model):
