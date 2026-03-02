8b8f349b404948f73b0657d79751c9ff5bceaebb
Hiroki <hiroki.watanabe@student.ateneo.edu>
2026-03-02 17:59:15 +0800

    adhered to PEP8 standards, made migrations and migrated

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
-from .models import Recipe, Ingredient, RecipeIngredient
+from .models import Recipe, RecipeIngredient
+
 
 class RecipeIngredientInLine(admin.TabularInline):
     model = RecipeIngredient
+
 
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
     list_display = ('name',)
     search_fields = ('name',)
     inlines = [RecipeIngredientInLine,]
+
 
 admin.site.register(Recipe, RecipeAdmin)
--- ledger/views.py
+++ ledger/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from .models import Recipe
 
+
 class RecipeListView(ListView):
     model = Recipe
-    template_name = "recipe_list.html" 
+    template_name = "recipe_list.html"
+
 
 class RecipeDetailView(DetailView):
     model = Recipe
-    template_name = "recipe_detail.html" 
-
+    template_name = "recipe_detail.html"
